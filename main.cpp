64a0b49ffe825bef54c5c09552e198296eb61850
El-Mathematica <menongokuls@gmail.com>
2022-07-22 14:48:06 +0100

    initial setup of files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <string>
+
+
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "hello" << endl;
+    getchar();
+}
