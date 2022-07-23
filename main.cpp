868382e1291a944e43a106ee4ee81fa95013056e
El-Mathematica <menongokuls@gmail.com>
2022-07-23 17:24:33 +0100

    implemented handling file/directory function

--- main.cpp
+++ main.cpp
@@ -2,12 +2,22 @@
 #include <cstdio>
 #include <fstream>
 #include <string>
-
+#include <filesystem>
 
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    cout << "hello" << endl;
+
+    string fileOrDir(argv[1]); //file or dir to be compiled
+
+    if(fileOrDir.find(".") != string::npos) { //checking whether its a file or dir
+        string fileBaseName = fileOrDir.substr(0, fileOrDir.find(".")); //file name without extension
+
+    } else {
+        string path(filesystem::current_path().string() + "\\fileOrDir");
+        cout << path;
+    }
+
     getchar();
 }
